27e774b77c8309b6a2310d4869fb33aabf9ebd12
shivanipt <87143211+shivanipt@users.noreply.github.com>
2021-08-14 10:52:40 -0500

    Add files via upload

--- Docking Undocking/sketch.js
+++ Docking Undocking/sketch.js
@@ -1,78 +1,80 @@
-var background, backgroundImg;
+var backgroundImg;
 var astronaut;
-var astroBathImg;
-var astroBrushImg;
-var astroDrinkImg;
-var astroEatImg;
-var astroGym1Img;
-var astroGym12Img;
-var astroMoveImg;
-var astroSleepImg;
+var bath;
+var brush;
+var eat;
+var gym;
+var move;
+var sleep;
 
 function preload() {
-  backgroundImg = loadImage("iss.png");
-  astroBathImg = loadAnimation("bath1.png", "bath2.png");
-  astroBrushImg = loadImage("brush.png");
-  astroDrinkImg = loadAnimation("drink1.png", "drink2.png");
-  astroEatImg = loadAnimation("eat1.png", "eat2.png");
-  astroGym1Img = loadAnimation("gym1.png", "gym2.png");
-  astroGym12Img = loadAnimation("gym11.png", "gym12.png");
-  astroMoveImg = loadAnimation("move.png", "move1.png");
-  astroSleepImg = loadImage("sleep.png");
+  backgroundImg = loadImage("./iss.png");
+  bath = loadAnimation("./bath1.png", "./bath1.png", "./bath1.png", "./bath2.png", "./bath2.png", "./bath2.png");
+  brush = loadAnimation("./brush.png");
+  eat = loadAnimation("./eat1.png", "./eat1.png", "./eat2.png", "./eat2.png");
+  gym = loadAnimation("./gym1.png", "./gym1.png", "./gym2.png", "./gym2.png");
+  move = loadAnimation("./move.png", "./move.png", "./move1.png", "./move1.png");
+  sleep = loadAnimation("./sleep.png");
 }
 
 function setup() {
   createCanvas(800,400);
-  background = createSprite(400, 200, 50, 50);
-  background.addImage("background", backgroundImg);
+  //background = createSprite(400, 200, 50, 50);
+  //background.addImage("background", backgroundImg);
 
   astronaut = createSprite(300,230);
-  astronaut.addImage(astroSleepImg);
+  astronaut.addAnimation("sleeping", sleep);
   astronaut.scale = 0.1;
 }
 
 function draw() {
-  //background(255,255,255);  
+  background(backgroundImg);  
+
+  drawSprites();
+
+  var edges = createEdgeSprites();
+
+  astronaut.bounceOff(edges);
 
   if(keyDown("UP_ARROW")){
-    astronaut.addImage(astroBrushImg);
-    astronaut.changeImage(astroBrushImg);
+    astronaut.addAnimation("brushing", brush);
+    astronaut.changeAnimation("brushing");
     astronaut.y = 350;
     astronaut.velocityX = 0;
     astronaut.velocityY = 0;
   }
 
   if(keyDown("DOWN_ARROW")) {
-    astronaut.changeAnimation(astroGym1Img);
+    astronaut.addAnimation("gymming", gym);
+    astronaut.changeAnimation("gymming");
     astronaut.y = 350;
-    astronaut.velocityX = 1;
-    astronaut.velocityY = 1;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
   }
 
   if(keyDown("LEFT_ARROW")) {
-    astronaut.addAnimation(astroBathImg);
-    astronaut.changeAnimation(astroBathImg);
+    astronaut.addAnimation("eating", eat);
+    astronaut.changeAnimation("eating");
+    astronaut.x = 150;
     astronaut.y = 350;
-    astronaut.velocityX = 2;
-    astronaut.velocityY = 2;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
   }
 
   if(keyDown("RIGHT_ARROW")) {
-    astronaut.addAnimation(astroEatImg);
-    astronaut.changeAnimation(astroEatImg);
+    astronaut.addAnimation("bathing", bath);
+    astronaut.changeAnimation("bathing");
     astronaut.y = 350;
     astronaut.velocityX = 1;
     astronaut.velocityY = 1;
   }
 
-  if(keyDown("M_KEY")) {
-    astronaut.addAnimation(astroMoveImg);
-    astronaut.changeAnimation(astroMoveImg);
-    astronaut.y = 350;
-    astronaut.velocityX = 3;
-    astronaut.velocityY = 3;
+  if(keyDown("M")) {
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    //astronaut.y = 350;
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
   }
 
-  createEdgeSprites();
-  drawSprites();
 }
